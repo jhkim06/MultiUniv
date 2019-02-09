5bc6f8ca8477bc9abe2f2105539d40c9bbaa368a
babo Haha <yourmailID@babo.com>
2019-02-09 15:47:00 +0900

    batchCare.sh

--- CommonTools/script/batchCare.sh
+++ CommonTools/script/batchCare.sh
@@ -2,6 +2,9 @@ if [[ $HOSTNAME == *"cms.snu.ac.kr"* ]]; then
   qstat -u "*"
 elif [[ $HOSTNAME == *"cms.snu.ac.kr"* ]]; then
   qstat
+elif [[ $HOSTNAME == *"ui"*".sdfarm.kr"* ]]; then
+  condor_q
+  condor_q -hold
 else
   echo "Not prepared at " $HOSTNAME
 fi
