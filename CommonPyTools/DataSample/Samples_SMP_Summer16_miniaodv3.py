db0c2c1fd6b030e522b70593c2a46f1afc9443b7
Junho Kim <jhkim@cern.ch>
2019-03-06 01:56:09 +0900

    add 2016 sample info

---
+++ CommonPyTools/DataSample/Samples_SMP_Summer16_miniaodv3.py
@@ -0,0 +1,10 @@
+# alias PD xsec nmc sumw
+sampleInfo = {
+'DYJets10to50_MG'		: {'name': 'DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8',	'xsec': 18610,		'nMC': 40364234,	'Nsum': 29374008 },
+'DYJets'		: {'name': 'DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8',		'xsec': 6225.42,	'nMC': 120777245,	'Nsum': 80924255 },
+'TT_powheg'		: {'name': 'TT_TuneCUETP8M2T4_13TeV-powheg-pythia8',				'xsec': 831.76,		'nMC': 76915549,	'Nsum': 76915549 },
+'WJets_MG'		: {'name': 'WJetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8',			'xsec': 61526.7,	'nMC': 57402435,	'Nsum': 57402435 },
+'WW_pythia'		: {'name': 'WW_TuneCUETP8M1_13TeV-pythia8',					'xsec': 118.7,		'nMC': 6988168,		'Nsum':	6988168  },
+'WZ_pythia'		: {'name': 'WZ_TuneCUETP8M1_13TeV-pythia8',					'xsec': 47.13,		'nMC': 3997571,		'Nsum':	3997571  },
+'ZZ_pythia'		: {'name': 'ZZ_TuneCUETP8M1_13TeV-pythia8',					'xsec': 16.523,		'nMC': 990064,		'Nsum':	990064  },
+}
