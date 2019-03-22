fdf43cd821d596e75b3c9ebfdb0cba7228be9ed2
SangEun Lee <d4space@gmail.com>
2019-03-22 19:18:07 +0900

    CommonTools added

---
+++ CommonPyTools/python/CommonTools.py
@@ -0,0 +1,111 @@
+import os
+
+from CommonPyTools.DataSample.SampleDef import *
+
+
+def AddOptions (parser):
+  parser.add_argument('-a', dest='Analyzer', default="")
+  parser.add_argument('-i', dest='InputSampleKey', default="")
+  parser.add_argument('-p', dest='DataPeriod', default="ALL")
+  parser.add_argument('-l', dest='InputSampleKeyList', default="")
+  parser.add_argument('-n', dest='NJobs', default=1, type=int)
+  parser.add_argument('--debug', dest='debug', default=0, type=int)
+  parser.add_argument('-o', dest='Outputdir', default="")
+  parser.add_argument('-q', dest='Queue', default="fastq")
+  parser.add_argument('-y', dest='Year', default="2017")
+  parser.add_argument('--overWrite',default=False, action='store_true')
+  parser.add_argument('--pycfg', dest='pycfg', default="configuration.py")
+  parser.add_argument('--sampleCfg', dest='sampleCfg', default="")
+  parser.add_argument('--plotCfg', dest='plotCfg', default="")
+  parser.add_argument('--variableCfg', dest='variableCfg', default="")
+  parser.add_argument('--cutCfg', dest='cutCfg', default="")
+  parser.add_argument('--lumi', dest='lumi', default=1, type=float)
+
+def findOptAndChange(parser, opt_name, opt_value):
+  args = parser.parse_args()
+  for action in parser._actions:
+    if action.dest == opt_name:
+      action.default = opt_value
+      return True
+  return False
+
+def LoadOptDefaults(parser):
+  args = parser.parse_args()
+  pycfg = args.pycfg
+  if os.path.exists(pycfg):
+    print 'it exists',pycfg
+    infile = open(pycfg,'r')
+    variables = {}
+    exec(infile,variables)
+    infile.close()
+    for opt_name, opt_value in variables.iteritems():
+      if opt_name[0] == '_': continue
+      #print opt_name, opt_value
+      if findOptAndChange(parser, opt_name, opt_value) :
+	print 'Option found and changed', opt_name, opt_value
+  else:
+    print 'there is no configuration file', pycfg, 'exiting....'
+    exit()
+
+
+def GetInputSamples(InputSampleKeys,DataPeriod,Year,Category,ProductionKey):
+  Dataperiods = DataPeriods(Year)
+  InputSamples = {}
+  StringForHash = ""
+  print 'File to import', Productions[Category][ProductionKey]['MC']
+  cmd = 'from '+Productions[Category][ProductionKey]['MC'] +' import *'
+  exec(cmd, globals())
+
+  for key in InputSampleKeys:
+    if key in DATaSets:
+      if DataPeriod=="ALL":
+        print key, Year,'ALL', Dataperiods
+        for period in Dataperiods:
+          InputSamples[key+":"+period]={'key':key}
+          StringForHash += key+":"+period
+      elif DataPeriod in Dataperiods:
+        print key, Year, DataPeriod
+        InputSamples[key+":"+DataPeriod]={'key':key}
+        StringForHash += key+":"+DataPeriod
+    else:
+      InputSamples[sampleInfo[key]['name']]={'key':key}
+      StringForHash += key
+
+  return InputSamples, StringForHash
+
+
+def GetOutDirBase(Category,ProductionKey, Outputdir,InSkimString,Userflags):
+  ### if Outputdir is not set, go to default setting
+  if Outputdir=="":
+    if InSkimString == "":
+      OutBase = Productions[Category][ProductionKey]['SkimDir']+'/'
+    else:
+      OutBase = Productions[Category][ProductionKey]['SkimDir']+'/'+InSkimString
+  else:
+    if Outputdir[0]!='/':
+      ## Add Abosolute path for outputdir
+      Outputdir = os.getcwd()+'/'+Outputdir
+    OutBase = Outputdir
+    if InSkimString != "":
+      OutBase = OutBase+'_'+InSkimString
+  
+  IsFirstFlag=True
+  for flag in Userflags:
+    if IsFirstFlag:
+      IsFirstFlag=False
+      if InSkimString == "":
+        OutBase += flag
+      else:
+        OutBase += '_'+flag
+    else:
+      OutBase += '_'+flag
+
+  return OutBase
+
+def GetOutDir(OutBase, SampleKey, DataPeriod):
+  if DataPeriod != "":
+    OutDir = OutBase + '/' + SampleKey + '_' + DataPeriod 
+  else:
+    OutDir = OutBase + '/' + SampleKey
+  return OutDir
+  
