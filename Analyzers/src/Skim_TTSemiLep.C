edba3e1d61fa17e8cefba592df503436eb595994
Byung-Hune Oh <bh.oh@cern.ch>
2019-04-01 16:08:23 +0900

    add Skim TTSemiLep

---
+++ Analyzers/src/Skim_TTSemiLep.C
@@ -0,0 +1,179 @@
+#include "Skim_TTSemiLep.h"
+
+void Skim_TTSemiLep::initializeAnalyzer(){
+
+  //initializeAnalyzerTools();
+  //=================================
+  // Skim Types
+  //=================================
+  if( HasFlag("L")){
+    cout<<"[Skim_TTSemiLep::initializeAnalyzer] Single Lepton Skim"<<endl;}
+  else{
+    cout <<"[Skim_TTSemiLep::executeEvent] Not ready for this Flags ";
+    for(unsigned int i=0; i<Userflags.size(); i++){
+      cout <<"  "<< Userflags.at(i);
+    }
+    cout<<endl;
+    exit(EXIT_FAILURE);
+  }
+
+  outfile->mkdir("recoTree");
+  outfile->cd("recoTree");
+  newtree = fChain->CloneTree(0);
+
+  // New Branch
+  TBranch* b_baseW =(TBranch*) newtree->GetListOfBranches()->FindObject("baseW");
+  Is_baseW = false;
+  if(b_baseW) {Is_baseW = true;}
+  if(Is_baseW == false){
+    newtree->Branch("baseW", &baseW,"baseW/D");
+  }
+
+
+
+}
+
+void Skim_TTSemiLep::executeEvent(){
+
+  muons.clear();
+  electrons.clear();
+
+
+  if(Is_baseW == false){
+    newtree->SetBranchAddress("baseW",&baseW);
+  }
+
+
+  FillHist("CutFlow",0,1,30,0,30);
+
+  // Filters ====================
+  if( HasFlag("MetFt"))if(!PassMETFilter()) return;
+  FillHist("CutFlow",1,1,30,0,30);
+
+  //XXX pt cuts are different from Skim_Leptons.C
+  //XXX electron ID is different from Skim_Leptons.C
+  muons=GetMuons("POGLoose",15.,2.4);
+  electrons=GetElectrons("passVetoID",15.,2.5); 
+
+  //std::sort(muons.begin(),muons.end(),PtComparing); //PtComaring @ AnalyzerCore.h
+
+  // dilepton condition
+
+  if( HasFlag("L")){
+    if (muons.size() + electrons.size() < 1) return;
+    FillHist("CutFlow",2,1,30,0,30);
+  }
+  else{
+    cout <<"[Skim_TTSemiLep::executeEvent] Not ready for this Flags ";
+    for(unsigned int i=0; i<Userflags.size(); i++){
+      cout <<"  "<< Userflags.at(i);
+    }
+    cout<<endl;
+    exit(EXIT_FAILURE);
+  }
+
+
+  evt = new Event;
+  *evt = GetEvent();
+
+  if(Is_baseW == false){
+    if(IsDATA){
+      baseW = 1;
+    }else{
+      baseW = weight_norm_1invpb*evt->MCweight()*evt->GetTriggerLumi("Full");
+    }
+  }
+
+
+  newtree->Fill();
+
+  return;
+
+
+}
+
+
+void Skim_TTSemiLep::executeEventFromParameter(AnalyzerParameter param){
+
+}
+
+Skim_TTSemiLep::Skim_TTSemiLep(){
+
+}
+
+Skim_TTSemiLep::~Skim_TTSemiLep(){
+
+}
+
+void Skim_TTSemiLep::WriteHist(){
+
+  //outfile->mkdir("recoTree");
+  //outfile->cd("recoTree"); Already at Skim_TTSemiLep::initializeAnalyzer
+  newtree->AutoSave();
+  //newtree->Write();
+  outfile->cd();
+  for(std::map< TString, TH1D* >::iterator mapit = maphist_TH1D.begin(); mapit!=maphist_TH1D.end(); mapit++){
+    TString this_fullname=mapit->second->GetName();
+    TString this_name=this_fullname(this_fullname.Last('/')+1,this_fullname.Length());
+    TString this_suffix=this_fullname(0,this_fullname.Last('/'));
+    TDirectory *dir = outfile->GetDirectory(this_suffix);
+    if(!dir){
+      outfile->mkdir(this_suffix);
+    }
+    outfile->cd(this_suffix);
+    mapit->second->Write(this_name);
+  }
+  for(std::map< TString, TH2D* >::iterator mapit = maphist_TH2D.begin(); mapit!=maphist_TH2D.end(); mapit++){
+    TString this_fullname=mapit->second->GetName();
+    TString this_name=this_fullname(this_fullname.Last('/')+1,this_fullname.Length());
+    TString this_suffix=this_fullname(0,this_fullname.Last('/'));
+    TDirectory *dir = outfile->GetDirectory(this_suffix);
+    if(!dir){
+      outfile->mkdir(this_suffix);
+    }
+    outfile->cd(this_suffix);
+    mapit->second->Write(this_name);
+  }
+
+  outfile->cd();
+  for(std::map< TString, std::map<TString, TH1D*> >::iterator mapit=JSmaphist_TH1D.begin(); mapit!=JSmaphist_TH1D.end(); mapit++){
+
+    TString this_suffix = mapit->first;
+    std::map< TString, TH1D* > this_maphist = mapit->second;
+
+
+    TDirectory *dir = outfile->GetDirectory(this_suffix);
+    if(!dir){
+      outfile->mkdir(this_suffix);
+    }
+    outfile->cd(this_suffix);
+
+    for(std::map< TString, TH1D* >::iterator mapit = this_maphist.begin(); mapit!=this_maphist.end(); mapit++){
+      mapit->second->Write();
+    }
+
+    outfile->cd();
+
+  }
+
+  for(std::map< TString, std::map<TString, TH2D*> >::iterator mapit=JSmaphist_TH2D.begin(); mapit!=JSmaphist_TH2D.end(); mapit++){
+
+    TString this_suffix = mapit->first;
+    std::map< TString, TH2D* > this_maphist = mapit->second;
+
+    TDirectory *dir = outfile->GetDirectory(this_suffix);
+    if(!dir){
+      outfile->mkdir(this_suffix);
+    }
+    outfile->cd(this_suffix);
+
+    for(std::map< TString, TH2D* >::iterator mapit = this_maphist.begin(); mapit!=this_maphist.end(); mapit++){
+      mapit->second->Write();
+    }
+
+    outfile->cd();
+
+  }
+
+}
+
---
+++ Gardener/test/Skim_TTSemiLep.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# --userflags a,b,c,... to be used inside of analyser
+#############################
+# userflags for Skim
+# DoubleLepTrg
+#############################
+
+# options: see mkGardener for more details
+
+# --dry_run :Dry run, job will not be submitted.
+# --MonitJob True: default False
+# -n : number of jobs in SNU, it is 999999 whatever number you put in.
+# --nTotFiles : number of files to run
+: '
+for sample in SingleMuon SingleElectron TTLJ_powheg TTLL_powheg TTJJ_powheg DYJets10to50_MG DYJets WJets_MG WW_pythia WZ_pythia ZZ_pythia ttW ttZ
+do
+  echo $sample
+  ./mkGardener.py --Category TTSemiLep -a Skim_TTSemiLep -y 2017 -i $sample  -n 50 --nTotFiles 1  --userflags MetFt,L --skimV 9 --dry_run 
+done
+'
+for sample in SingleMuon SingleElectron TT_powheg DYJets10to50_MG DYJets WJets_MG WW_pythia WZ_pythia ZZ_pythia
+do
+  echo $sample
+  ./mkGardener.py --Category TTSemiLep -a Skim_TTSemiLep -y 2016 -i $sample  -n 50 --nTotFiles 1  --userflags MetFt,L --skimV 9 --dry_run 
+done
+#for sample in DoubleEG DoubleMuon DYJets10to50_MG DYJets TT_powheg WJets_MG WW_pythia WZ_pythia ZZ_pythia 
+#do
+#  echo $sample
+#  ./mkGardener.py --Category SMP -a Skim_TTSemiLep -y 2016 -i $sample  -n 500 --nTotFiles 1  --userflags MetFt,L --skimV 9 --dry_run 
+#done
+
