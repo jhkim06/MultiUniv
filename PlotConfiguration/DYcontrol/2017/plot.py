62db61b458c450d0e3a94d14baa849335bf9f83a
SangEun Lee <d4space@gmail.com>
2019-04-04 16:10:30 +0900

    making plots

--- PlotConfiguration/DYcontrol/2017/plot.py
+++ PlotConfiguration/DYcontrol/2017/plot.py
@@ -2,14 +2,39 @@ from CommonPyTools.python.CommonTools import *
 
 # Signal only has the style, the other background has style 3001
 # lineWidth 2, this is another option
+# style 0 : noFillColor
+
+groupPlot['WJet'] = {
+    'nameHR' : "WJet",
+    'isSignal' : 0,
+    'color':  616,
+    'samples' : ['WJets_MG']
+    }
+
+groupPlot['TT'] = {
+    'nameHR' : "TT",
+    'isSignal' : 0,
+    'color':  600,
+    'samples' : ['TTLL_powheg']
+    }
 
 groupPlot['VV'] = {
     'nameHR' : "VV",
     'isSignal' : 0,
-    'color':  458,
-    'samples' : ['WW','WZ']
+    'color':  432,
+    'samples' : ['WW_pythia','WZ_pythia','ZZ_pythia']
     }
 
+# kOrange 800
+groupPlot['DY'] = {
+    'nameHR' : "DY",
+    'isSignal' :0,
+    'color': 798, 
+    'style': 4050,
+    'lineColor':807,
+    'lineWidth':1,
+    'samples' : ['DYJets']
+    }
 
 #plot['DYJets10to50_MG'] = {
 #    'color': 418,
@@ -21,7 +46,7 @@ groupPlot['VV'] = {
 #    }
 #
 plot['DYJets'] = {
-    'color': 400, # Yellow
+    'color': 400,
     'isSignal' :1,
     'isData': 0,
     'style': 4050,
@@ -42,6 +67,20 @@ plot['WW_pythia'] = {
     'plotName':'WW',
     'scale':1,
     }
+plot['TTLL_powheg'] = {
+    'color': 550,
+    'isSignal' :0,
+    'isData': 0,
+    'plotName':'TT',
+    'scale':1,
+    }
+plot['WJets_MG'] = {
+    'color': 550,
+    'isSignal' :0,
+    'isData': 0,
+    'plotName':'WJets',
+    'scale':1,
+    }
 
 plot['DoubleEG'] = {
     'color':1,
@@ -53,3 +92,11 @@ plot['DoubleEG'] = {
 
 legend['lumi'] = 'L = 41.5/fb'
 legend['sqrt'] = '#sqrt{s} = 13 TeV'
+# extraText default is Preliminary
+#legend['extraText'] = 'Preliminary'
+# iPeriod = 1*(0/1 7 TeV) + 2*(0/1 8 TeV)  + 4*(0/1 13 TeV)
+# For instance: 
+#               iPeriod = 3 means: 7 TeV + 8 TeV
+#               iPeriod = 0 means: free form (uses lumi_sqrtS)
+legend['iPeriod'] = 0
+legend['iPos'] = 0
--- PlotConfiguration/DYcontrol/2017/run.sh
+++ PlotConfiguration/DYcontrol/2017/run.sh
@@ -1,18 +1,17 @@
 
 
-#####################################################
-#            _     _____ _                      
-#           | |   / ____| |                     
-#  _ __ ___ | | _| (___ | |__   __ _ _ __   ___ 
-# | '_ ` _ \| |/ /\___ \| '_ \ / _` | '_ \ / _ \
-# | | | | | |   < ____) | | | | (_| | |_) |  __/
-# |_| |_| |_|_|\_\_____/|_| |_|\__,_| .__/ \___|
-#                                   | |         
-#                                   |_|         
+###################################################################################
 #
-#####################################################
-
-# Options
+#            _     _____ _                         ____        _   _             
+#           | |   / ____| |                       / __ \      | | (_)            
+#  _ __ ___ | | _| (___ | |__   __ _ _ __   ___  | |  | |_ __ | |_ _  ___  _ __  
+# | '_ ` _ \| |/ /\___ \| '_ \ / _` | '_ \ / _ \ | |  | | '_ \| __| |/ _ \| '_ \ 
+# | | | | | |   < ____) | | | | (_| | |_) |  __/ | |__| | |_) | |_| | (_) | | | |
+# |_| |_| |_|_|\_\_____/|_| |_|\__,_| .__/ \___|  \____/| .__/ \__|_|\___/|_| |_|
+#                                   | |                 | |                      
+#                                   |_|                 |_|                      
+#
+###################################################################################
 # --debug 0 default, 1 INFO, 2 DEBUG
 # -q :Queue tamsa1(fastq)
 # --dry_run : no execution
@@ -22,17 +21,22 @@
 # --cleanUp
 # --onlyVariable=mll
 
-#for sample in DYJets10to50_MG
-##for sample in DoubleEG DoubleMuon DYJets10to50_MG DYJets TTLL_powheg WJets_MG WW_pythia WZ_pythia ZZ_pythia 
-#do
-#  echo $sample
-#  mkShapes.py  -i $sample -n 12345 --nTotFiles 1  --overWrite --dry_run
-#  #checkGardenerBatch.py -a Skim_DiLep -y 2017 -i $sample -n 500 --nTotFiles 0 --RundirBase /data7/Users/salee/SKFlatRunlog/ --SkimName MetFt_L_v0_LL_v0_MuMuOrElEl_v0
-#  #./mkGardener.py --Category SMP -a hadd -y 2017 -i $sample -n 500 --nTotFiles 0 --InSkim MetFt_L_v0_LL_v0_MuMuOrElEl_v0 --userflags hadd --skimV 0 --dry_run
-#  #checkGardenerBatch.py -a hadd -y 2017 -i $sample -n 500 --nTotFiles 0 --RundirBase /data7/Users/salee/SKFlatRunlog/ --SkimName MetFt_L_v0_LL_v0_MuMuOrElEl_v0_hadd_v0
-#done
+
+###########################################################################
+#            _    _____  _       _                 _   _             
+#           | |  |  __ \| |     | |               | | (_)            
+#  _ __ ___ | | _| |__) | | ___ | |_    ___  _ __ | |_ _  ___  _ __  
+# | '_ ` _ \| |/ /  ___/| |/ _ \| __|  / _ \| '_ \| __| |/ _ \| '_ \ 
+# | | | | | |   <| |    | | (_) | |_  | (_) | |_) | |_| | (_) | | | |
+# |_| |_| |_|_|\_\_|    |_|\___/ \__|  \___/| .__/ \__|_|\___/|_| |_|
+#                                           | |                      
+#                                           |_|                      
+#
+###########################################################################
+# --scaleToPlot : default 2.0
+# --showIntegralLegend=1 default
 
 
 #mkShapes.py --pycfg  configuration.py -n 400 --nTotFiles 0  --overWrite --doBatch 
 #mkShapes.py --pycfg  configuration.py -n 400 --nTotFiles 0  --overWrite --doHadd --cleanUp
-mkPlot.py --pycfg configuration.py --inputFile=Output_MetFt_L_v0_LL_v0_MuMuOrElEl_v0_hadd_v0_DY/DY.root --onlyVariable=mll
+mkPlot.py --pycfg configuration.py --inputFile=Output_MetFt_L_v0_LL_v0_MuMuOrElEl_v0_hadd_v0_DY/DY.root --onlyVariable=mll --minLogC=1 --maxLogC=1000
