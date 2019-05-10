0b360d3102d7643587c10a48e06ac4dd6f1ddaf8
SangEun Lee <d4space@gmail.com>
2019-05-10 20:00:06 +0900

    tamsa1 env

--- setup_tmp.sh
+++ setup_tmp.sh
@@ -116,6 +116,41 @@ elif [[ $HOSTNAME == *"tamsa2"* ]]; then
   #source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.14.06/x86_64-centos7-gcc48-opt/bin/thisroot.sh
   #source /data6/Users/salee/ROOT616/bin/thisroot.sh
   #export PYTHONPATH=$PYTHONPATH:/data6/Users/salee/ROOTLib
+elif [[ $HOSTNAME == *"tamsa1"* ]]; then
+
+  echo "Working on tamsa1"
+  export SKFlatRunlogDir="/data6/Users/$USER/SKFlatRunlog/"
+  export SKFlatOutputDir="/data9/Users/salee/Higgs"
+  #export SKFlatOutputDir="/data7/Users/$USER/SKFlatOutput/"
+  export DATA_DIR=$SKFlat_WD/data/$SKFlatV
+
+
+  export GCC_HOME=/usr/
+  export PATH=$GCC_HOME/bin:$PATH
+  export LD_LIBRARY_PATH=$GCC_HOME/lib/gcc/x86_64-redhat-linux:$GCC_HOME/lib64:$LD_LIBRARY_PATH
+  source /data6/Users/salee/ROOT616/bin/thisroot.sh
+  #export LD_LIBRARY_PATH=$GCC_HOME/lib64:$GCC_HOME/lib:$LD_LIBRARY_PATH
+  #export LD_LIBRARY_PATH=/home/salee/lib/cling/lib/:$LD_LIBRARY_PATH
+  ##export PATH=/home/salee/lib/cling/bin:$PATH
+  #export CC=/opt/ohpc/pub/compiler/gcc/7.3.0/gcc
+  #export CXX=/opt/ohpc/pub/compiler/gcc/7.3.0/g++
+  #export CPP=/opt/ohpc/pub/compiler/gcc/7.3.0/cpp
+  #export LD=/opt/ohpc/pub/compiler/gcc/7.3.0/gcc
+  #alias  ld='/opt/ohpc/pub/compiler/gcc/7.3.0/gcc'
+  #alias  cc='/opt/ohpc/pub/compiler/gcc/7.3.0/gcc'
+  #source /data6/Users/salee/ROOT616/bin/thisroot.sh
+  #export PATH=/share/apps/python2.7/bin/:$PATH
+
+  #export CMS_PATH=/cvmfs/cms.cern.ch
+  #source $CMS_PATH/cmsset_default.sh
+  #export SCRAM_ARCH=slc6_amd64_gcc630
+  #cd /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_10_2_0/src/
+  #eval `scramv1 runtime -sh`
+  #cd -
+  #source /cvmfs/cms.cern.ch/slc6_amd64_gcc630/cms/cmssw/CMSSW_10_2_0/external/slc6_amd64_gcc630/bin/thisroot.sh
+  #source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.14.06/x86_64-centos7-gcc48-opt/bin/thisroot.sh
+  #source /data6/Users/salee/ROOT616/bin/thisroot.sh
+  #export PYTHONPATH=$PYTHONPATH:/data6/Users/salee/ROOTLib
 
 
 elif [[ $HOSTNAME == *"knu"* ]]; then
