4f25f8045a4e42549eac85d8a535b4d9d45b9458
Junho Kim <jhkim@cern.ch>
2019-05-23 22:54:33 +0900

    add Skim_Efficiency

---
+++ Analyzers/include/Skim_Efficiency.h
@@ -0,0 +1,59 @@
+#ifndef Skim_Efficiency_h
+#define Skim_Efficiency_h
+
+#include "RoccoR.h"
+#include "AnalyzerCore.h"
+#include "RootHelper.h"
+
+class Skim_Efficiency : public AnalyzerCore {
+
+public:
+  Skim_Efficiency();
+  ~Skim_Efficiency();
+
+  void initializeAnalyzer();
+  void executeEventFromParameter(AnalyzerParameter param);
+  void executeEvent();
+
+  Event* evt;
+
+  TTree *newtree;
+
+  void WriteHist();
+
+private:
+
+  std::map<TString, double>  Mu_IdSF;
+  std::map<TString, double>  MuMu_IdSF;
+  std::map<TString, double>  Mu_IsoSF;
+  std::map<TString, double>  MuMu_IsoSF;
+  std::map<TString, double>  Mu_TriggerSF;
+  std::map<TString, double>  MuMu_TriggerSF;
+  std::map<TString, TString> Mu_IdSF_Iso;
+  std::map<TString, TString> Mu_IdSF_Trigger;
+  std::map<TString, TString> MuMu_IdSF_Trigger;
+
+
+  std::map<TString, double>  El_IdSF;
+  std::map<TString, TString> El_IdSF_key;
+  std::map<TString, double>  El_TriggerSF;
+  std::map<TString, TString> El_IdSF_Trigger;
+  std::map<TString, double>  El_RecoSF;
+
+  std::map<TString, double>  ElEl_IdSF;
+  std::map<TString, double>  ElEl_TriggerSF;
+  std::map<TString, TString> ElEl_IdSF_Trigger;
+  std::map<TString, double>  ElEl_RecoSF;
+
+  std::vector<Muon> muons;
+  std::vector<Electron> electrons;
+  std::vector<Lepton*> leps;
+
+  TString ElectronID_key;
+
+};
+
+
+
+#endif
+
