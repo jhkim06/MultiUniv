c265b748b0af836115960b78757783bb113bddc5
Byung Hun, Oh <bh.oh@cern.ch>
2019-05-28 15:46:04 +0900

    add Skim_K2

--- Analyzers/include/Analyzers_LinkDef.h
+++ Analyzers/include/Analyzers_LinkDef.h
@@ -12,14 +12,16 @@
 #pragma link C++ class SKFlatValidation+;
 #pragma link C++ class SkimTree_LRSMHighPt+;
 #pragma link C++ class Skim_Leptons+;
-#pragma link C++ class Skim_K1+;
 #pragma link C++ class Skim_DiLep+;
-#pragma link C++ class Skim_TTSemiLep+;
 #pragma link C++ class SMP_Validation+;
 #pragma link C++ class Skim_ISRUnfoldInput+;
 #pragma link C++ class Skim_FakeEst+;
 #pragma link C++ class Module_Efficiency+;
 
 #pragma link C++ class Skim_TSCorrTree+;
+#pragma link C++ class Skim_TTSemiLep+;
+
+#pragma link C++ class Skim_K1+;
+#pragma link C++ class Skim_K2+;
 
 #endif
