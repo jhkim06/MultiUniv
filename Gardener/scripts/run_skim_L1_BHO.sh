a3181acd8d5d2ebf372711bc3e1993a3bbf86517
Byung Hun, Oh <bh.oh@cern.ch>
2019-07-19 13:21:06 +0900

    missing files

--- Gardener/scripts/run_skim_L1_BHO.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for sample in DoubleEG DoubleMuon SingleElectron SingleMuon
-do
-  ./mkGardener.py --Category SMP -a Skim_Leptons -y 2017 -i $sample  -n 300 --nTotFiles 1  --userflags MetFt,L --skimV 2
-  sleep 2h
-done
-
-
