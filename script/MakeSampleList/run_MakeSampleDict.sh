1acb5ce2e3decc9cf36aa068a398576b6f8575d8
Byung Hun, Oh <bh.oh@cern.ch>
2019-07-24 15:05:27 +0900

    Run2Legacy_v3 sample definition in python dictionary

--- script/MakeSampleList/run_MakeSampleDict.sh
+++ script/MakeSampleList/run_MakeSampleDict.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# to make 2016 bkg sample lists
+#./MakeSampleDict.py --in 'SampleSummary_MC_2016.txt' --out 'Samples_SMP_Summer16_miniaodv3.py'
+
+# to make 2017 bkg sample lists
+./MakeSampleDict.py --in 'SampleSummary_MC_2017.txt' --out 'Samples_SMP_Fall17_miniaodv2.py'
+
+# to make 2018 bkg sample lists
+#./MakeSampleDict.py --in 'SampleSummary_MC_2018.txt' --out 'Samples_SMP_Autumn18_miniaodv3.py'
+
+
+
 # to make CH sample lists
 #./MakeSampleDict.py --in 'SampleSummary_Signal_CHToCB.txt' --out 'Samples_CHToCB_Fall17_miniaodv2.py'
 
 
 # to make TT systematic lists
-./MakeSampleDict.py --in 'SampleSummary_ttbar_syst.txt' --out 'Samples_ttbar_syst_Fall17_miniaodv2.py'
+#./MakeSampleDict.py --in 'SampleSummary_ttbar_syst.txt' --out 'Samples_ttbar_syst_Fall17_miniaodv2.py'
