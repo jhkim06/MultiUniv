78e2035d175b638281eecdfeffb4b1c9d6beb8e1
Byung Hun, Oh <bh.oh@cern.ch>
2019-09-23 13:25:32 +0900

    add cutflow config

---
+++ PlotConfiguration/CHToCBcutflow/2018/cuts_Mu.py
@@ -0,0 +1,6 @@
+from CommonPyTools.python.CommonTools import *
+SKFlat_WD = os.getenv('SKFlat_WD')
+sys.path.insert(0,SKFlat_WD+'/CommonTools/include') 
+from Definitions import *
+
+cuts['Mu'] = ''
---
+++ PlotConfiguration/CHToCBcutflow/2018/nuisances.py
@@ -0,0 +1,8 @@
+nuisances['5%_variation'] = {
+    'name' : '5%_variation',
+    'type' : 'lnN',
+    'all'  : {
+      'value' : 1.05,
+     }
+}
+
