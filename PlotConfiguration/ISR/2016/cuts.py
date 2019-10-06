866bd779226373215c0cb2ce8a0f522d2d7f8963
Junho Kim <jhkim@cern.ch>
2019-10-06 15:22:29 +0900

    add fake rate root files

--- PlotConfiguration/ISR/2016/cuts.py
+++ PlotConfiguration/ISR/2016/cuts.py
@@ -3,8 +3,8 @@ SKFlat_WD = os.getenv('SKFlat_WD')
 sys.path.insert(0,SKFlat_WD+'/CommonTools/include') 
 from Definitions import *
 
-supercut = 'evt_tag_dimuon_rec == 1 && evt_tag_analysisevnt_sel_rec == 1 && dilep_mass_rec > 40'
+supercut = '1==1'
 
-cuts['dipt_cut'] = 'dilep_pt_rec < 100'
+cuts['detector_level'] = 'evt_tag_dielectron_rec == 1 && evt_tag_analysisevnt_sel_rec == 1 && dilep_pt_rec < 100.'
 
 cuts['dimu_photon_cut'] = 'photon_n_rec > 0 && dilep_photon_mass_rec > 85 && dilep_photon_mass_rec < 105 && dilep_mass_rec > 30 && dilep_mass_rec < 87'
