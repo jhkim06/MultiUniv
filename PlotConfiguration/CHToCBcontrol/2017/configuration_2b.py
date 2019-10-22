0743b3b85bda2bc990026bab65604976b6fa17b0
Byung Hun, Oh <bh.oh@cern.ch>
2019-10-22 11:11:36 +0900

    tmp 2017 plot config

--- PlotConfiguration/CHToCBcontrol/2017/configuration_2b.py
+++ PlotConfiguration/CHToCBcontrol/2017/configuration_2b.py
@@ -5,7 +5,6 @@ Category    = 'SMP'
 Year        = '2017'
 lumi        = 41.527540
 InSkim      = 'MetFt_L_v2_TTSemiLep_v1_K2_v1'
-#InSkim      = 'MetFt_L_v1_TTSemiLep_v3_K2_v1'
 # Userflags: separate by ','
 Userflags   = 'CHToCB_2b' # flag appends to ouput directory name
 #Userflags   = 'AlPhaS'
--- PlotConfiguration/CHToCBcontrol/2017/run.sh
+++ PlotConfiguration/CHToCBcontrol/2017/run.sh
@@ -68,15 +68,15 @@
 #sleep 30m
 #mkShapes.py --pycfg  configuration_2b.py -n 10 --nTotFiles 0  --overWrite --doHadd --cleanUp
 #sleep 5s
-mkPlot.py --pycfg configuration_Mu2b.py --inputFile=Output_MetFt_L_v1_TTSemiLep_v3_K2_v1_CHToCB_2b/CHToCB_2b.root  --minLogC=1 --maxLogC=1000 --showIntegralLegend=1
-sleep 5s
-mkPlot.py --pycfg configuration_El2b.py --inputFile=Output_MetFt_L_v1_TTSemiLep_v3_K2_v1_CHToCB_2b/CHToCB_2b.root  --minLogC=1 --maxLogC=1000 --showIntegralLegend=1
-sleep 5s
+#mkPlot.py --pycfg configuration_Mu2b.py --inputFile=Output_MetFt_L_v1_TTSemiLep_v3_K2_v1_CHToCB_2b/CHToCB_2b.root  --minLogC=1 --maxLogC=1000 --showIntegralLegend=1
+#sleep 5s
+#mkPlot.py --pycfg configuration_El2b.py --inputFile=Output_MetFt_L_v1_TTSemiLep_v3_K2_v1_CHToCB_2b/CHToCB_2b.root  --minLogC=1 --maxLogC=1000 --showIntegralLegend=1
+#sleep 5s
 
-#mkShapes.py --pycfg  configuration_3b.py -n 10 --nTotFiles 0  --overWrite --doBatch
+mkShapes.py --pycfg  configuration_3b.py -n 50 --nTotFiles 0  --overWrite --doBatch --multiQueue --concLimit 200
 #sleep 30m
 #mkShapes.py --pycfg  configuration_3b.py -n 10 --nTotFiles 0  --overWrite --doHadd --cleanUp
 #sleep 5s
-mkPlot.py --pycfg configuration_Mu3b.py --inputFile=Output_MetFt_L_v1_TTSemiLep_v3_K2_v1_CHToCB_3b/CHToCB_3b.root  --minLogC=1 --maxLogC=1000 --showIntegralLegend=1
-sleep 5s
-mkPlot.py --pycfg configuration_El3b.py --inputFile=Output_MetFt_L_v1_TTSemiLep_v3_K2_v1_CHToCB_3b/CHToCB_3b.root  --minLogC=1 --maxLogC=1000 --showIntegralLegend=1
+#mkPlot.py --pycfg configuration_Mu3b.py --inputFile=Output_MetFt_L_v1_TTSemiLep_v3_K2_v1_CHToCB_3b/CHToCB_3b.root  --minLogC=1 --maxLogC=1000 --showIntegralLegend=1
+#sleep 5s
+#mkPlot.py --pycfg configuration_El3b.py --inputFile=Output_MetFt_L_v1_TTSemiLep_v3_K2_v1_CHToCB_3b/CHToCB_3b.root  --minLogC=1 --maxLogC=1000 --showIntegralLegend=1
