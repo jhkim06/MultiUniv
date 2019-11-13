d0264efa8bd7e789560e66e96797e88e051333cf
Byung Hun, Oh <bh.oh@cern.ch>
2019-11-13 17:51:32 +0900

    ts-corr, double sided crystalball function

--- JetCorrection/TSCorrection/include/functions.h
+++ JetCorrection/TSCorrection/include/functions.h
@@ -2,18 +2,23 @@
 #define functions_H
 
 #include "TString.h"
+#include "TCanvas.h"
+#include "TLatex.h"
 
 #include <sstream>
 #include <string>
 #include <cstring>
 #include <iomanip>     //necessary for setprecision
 
+Double_t DoubleSidedSrystalFnc(Double_t* x, Double_t* par);
 Double_t CrystalFnc(Double_t* x, Double_t* par);
 Double_t ResponseFittingFunction(Double_t *x, Double_t *par);
 Double_t SigmaErrorFittingFunction(Double_t *x, Double_t *par);
 TString ResponseFittingFunction();
 TString SigmaErrorFittingFunction();
 TString Double_t_to_TString(Double_t num);
+
+void AddCMSSimulation(TCanvas* c1);
 
 #endif
 
