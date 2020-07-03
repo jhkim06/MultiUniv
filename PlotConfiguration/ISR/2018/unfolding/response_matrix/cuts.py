25beaea4f56d320011491c9c5e410aa89e4ea4cb
Junho Kim <jhkim@cern.ch>
2020-07-03 15:38:48 +0900

    2018 detector response script update

--- PlotConfiguration/ISR/2018/unfolding/response_matrix/cuts.py
+++ PlotConfiguration/ISR/2018/unfolding/response_matrix/cuts.py
@@ -4,20 +4,20 @@ sys.path.insert(0,SKFlat_WD+'/CommonTools/include')
 from Definitions import *
 
 # supercut will be applied last in the cuts
-#supercut = 'evt_tag_dielectron_promptfinal == 1 &&  evt_tag_dielectron_promptfinal == 1'
 supercut = 'evt_tag_dielectron_promptfinal == 1 && dilep_mass_FSRgamma_gen_ispromptfinal < 380. && dilep_pt_FSRgamma_gen_ispromptfinal < 110'
 
 #
-cuts['detector_level_DY_Fake'] = 'evt_tag_dielectron_rec_Nominal == 1 && evt_tag_analysisevnt_sel_rec_Nominal == 1 && pass_kinematic_cut_el_FSRgamma_gen == 0 && dilep_mass_rec_Nominal > 10. && dilep_mass_rec_Nominal < 380. && dilep_pt_rec_Nominal < 110.'
+cuts['detector_level_DY_Fake'] = 'evt_tag_dielectron_rec_Nominal == 1 && evt_tag_analysisevnt_sel_rec_Nominal == 1 && pass_kinematic_cut_el_FSRgammaDRp1_gen == 0 && dilep_mass_rec_Nominal > 45. && dilep_mass_rec_Nominal < 380. && dilep_pt_rec_Nominal < 110.'
 
 # Matrix
-cuts['Detector_Dressed_DRp1_Fiducial'] = 'pass_kinematic_cut_el_FSRgammaDRp1_gen == 1 && evt_tag_dielectron_rec_Nominal == 1 && evt_tag_analysisevnt_sel_rec_Nominal == 1 && dilep_mass_rec_Nominal < 380. && dilep_pt_rec_Nominal < 110.'
-cuts['Detector_Dressed_DRp1_FullPhase'] = 'evt_tag_dielectron_promptfinal == 1 && evt_tag_dielectron_rec_Nominal == 1 && evt_tag_analysisevnt_sel_rec_Nominal == 1 && dilep_mass_rec_Nominal < 380. && dilep_pt_rec_Nominal < 110.'
-cuts['Detector_Dressed_DR4PI_FullPhase'] = 'evt_tag_dielectron_promptfinal == 1 && evt_tag_dielectron_rec_Nominal == 1 && evt_tag_analysisevnt_sel_rec_Nominal == 1 && dilep_mass_rec_Nominal < 380. && dilep_pt_rec_Nominal < 110.'
-cuts['Detector_Dressed_DR4PI_Fiducial'] = 'pass_kinematic_cut_el_FSRgamma_gen == 1 && evt_tag_dielectron_rec_Nominal == 1 && evt_tag_analysisevnt_sel_rec_Nominal == 1 && dilep_mass_rec_Nominal > 10. && dilep_mass_rec_Nominal < 380. && dilep_pt_rec_Nominal < 110.'
+cuts['Detector_Dressed_DRp1_Fiducial'] = 'pass_kinematic_cut_el_FSRgammaDRp1_gen == 1 && evt_tag_dielectron_rec_Nominal == 1 && evt_tag_analysisevnt_sel_rec_Nominal == 1 && dilep_mass_rec_Nominal > 45. && dilep_mass_rec_Nominal < 380. && dilep_pt_rec_Nominal < 110.'
+cuts['Detector_Dressed_DRp1_FullPhase'] = 'evt_tag_dielectron_promptfinal == 1 && evt_tag_dielectron_rec_Nominal == 1 && evt_tag_analysisevnt_sel_rec_Nominal == 1 && dilep_mass_rec_Nominal > 45. && dilep_mass_rec_Nominal < 380. && dilep_pt_rec_Nominal < 110.'
+cuts['Detector_Dressed_DR4PI_FullPhase'] = 'evt_tag_dielectron_promptfinal == 1 && evt_tag_dielectron_rec_Nominal == 1 && evt_tag_analysisevnt_sel_rec_Nominal == 1 && dilep_mass_rec_Nominal > 45. && dilep_mass_rec_Nominal < 380. && dilep_pt_rec_Nominal < 110.'
+cuts['Detector_Dressed_DR4PI_Fiducial'] = 'pass_kinematic_cut_el_FSRgamma_gen == 1 && evt_tag_dielectron_rec_Nominal == 1 && evt_tag_analysisevnt_sel_rec_Nominal == 1 && dilep_mass_rec_Nominal > 45. && dilep_mass_rec_Nominal < 380. && dilep_pt_rec_Nominal < 110.'
 
 # For acceptance correction
 #cuts['FullPhase'] = 'evt_tag_dielectron_promptfinal == 1'
+#cuts['Acceptance'] = 'pass_kinematic_cut_el_FSRgammaDRp1_gen == 1'
 
 #cuts['detector_level_DY_Fake_NoLepMomCorr'] = 'evt_tag_dielectron_rec_NoLepMomCorr == 1 && evt_tag_analysisevnt_sel_rec_NoLepMomCorr == 1 && pass_kinematic_cut_el_FSRgamma_gen == 0 && dilep_mass_rec_NoLepMomCorr > 10. && dilep_mass_rec_NoLepMomCorr < 380. && dilep_pt_rec_NoLepMomCorr < 110.'
 #
